4fce65898716c64951dafd357e62aacc793ed928
Muneera <muneerakottarath99@gmail.com>
2024-08-12 11:01:46 +0530

    commited

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [newTodo,setNewTodo] = useState("")
+  const [todos,setTodos] = useState([])
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  }
+ 
+  const addTodo=()=>{
+    if(newTodo.trim() !== ""){
+      setTodos([...todos,newTodo])
+    }
+  }
+  const deleteTodo=(index)=>{
+    const afterdelete=todos.filter((element,i)=>i !== index);
+    setTodos(afterdelete);
+ }
+  return (
+  
+    <div className='container'>
+      <div className='todocontainer'>
+        <h2>To Do list</h2>
+        <form className='formm' onSubmit={handleSubmit}>
+        <input type="text" placeholder='+Add a task' className='inputli' value={newTodo} onChange={(event)=>setNewTodo(event.target.value)}/>
+        <button className='btn2' onClick={addTodo}>ADD</button>
+        </form>
+        <div className='list'>
+          <ol>
+             {todos.map((todo,index) =>
+             <li key={index} className='lii' >
+              <span className='text'>{todo}</span>
+              <button className='btn1' onClick={()=>deleteTodo(index)}>✔️</button>
+              </li>
+              
+            )}
+            
+          </ol>
+        </div>
+
+      </div>
+    </div>
+    
+    
+  )
+}
+
+export default App
